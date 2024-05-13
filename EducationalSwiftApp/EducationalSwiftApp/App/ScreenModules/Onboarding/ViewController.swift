5fc4cf150524d19ef6aa4ebcde3b9620052b4641
ArthurBJ <baybikov.artur@mail.ru>
2024-05-13 17:32:24 +0300

    Created base project, removed storyboard references

---
+++ EducationalSwiftApp/EducationalSwiftApp/App/ScreenModules/Onboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EducationalSwiftApp
+//
+//  Created by Артур Байбиков on 13.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
